ab31be85d3588d58554dfda5ebc97c6ef7cd6e79
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-09-05 11:55:18 -0600

    commit

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -5,11 +5,11 @@ public class ProjectA
 	public static void main(String[] args) 
 	{
 		Scanner userInput = new Scanner(System.in);
-		
 		System.out.println("Input any two words seperated by a space. I will print as a proper varriable name.");
+		
 		String words = userInput.nextLine();
-		int variable = words.length();
-		words = words.toLowerCase().substring(variable - 1).replace(" ", "").toUpperCase().substring(variable - 1).toLowerCase();
+		words = words.substring(0,1).toLowerCase() + words.substring(1).toLowerCase() + words.replace(" ", "") + words.substring(0,2).toUpperCase()+ words.substring(1).toLowerCase();
+		
 		System.out.println(words);
 	}
 }
