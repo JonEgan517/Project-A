ba8dd78d5c8a7ca814aefd4ea09ff823197471f5
paulegancpa <paulegancpa@DESKTOP-IM14J0V>
2019-09-04 19:32:42 -0600

    commit

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -8,7 +8,8 @@ public class ProjectA
 		
 		System.out.println("Input any two words seperated by a space. I will print as a proper varriable name.");
 		String words = userInput.nextLine();
-		words = words.toLowerCase().replace(" ", "").toUpperCase().toLowerCase();
+		int variable = words.length();
+		words = words.toLowerCase().substring(variable - 1).replace(" ", "").toUpperCase().substring(variable - 1).toLowerCase();
 		System.out.println(words);
 	}
 }
